367288eccf85d14ae62dffea3a3605b4350fdbea
Valimp <valentinchauvet.contact@gmail.com>
2024-04-12 17:24:42 +0200

    fix: details

--- src/components/TaskCounter.tsx
+++ src/components/TaskCounter.tsx
@@ -9,7 +9,12 @@ const TaskCounter = ({ tasksNumber }: TaskCounterProps) => {
     <div className='w-fit px-4 py-3 rounded-lg shadow-lg bg-zinc-200 fixed top-3 right-3'>
         <Typo type='p'>
           <div className="flex gap-1">
-            <span className="lg:inline-block hidden">Vous avez {tasksNumber} corvées à faire ! </span>
+            {
+              tasksNumber === 0 ? 
+              <span className="font-semibold">Vous avez aucune corvée</span> 
+              : 
+              <span className="font-semibold">Vous avez {tasksNumber} à faire</span>
+            }
             {tasksNumber === 0 ? 
               <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Face%20Holding%20Back%20Tears.png" alt="Face Holding Back Tears" width="25" height="25" />
               : 
